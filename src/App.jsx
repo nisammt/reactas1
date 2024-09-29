60bbc4286fcc377273a9e1dc41f3c4d2914cbb14
nisam <nisammt@gmail.com>
2024-09-29 19:08:07 +0530

    projectadded

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Navebar from './components/Navebar'
+import Product from './components/product'
+
+
+
+function App() {
+
+  
+  return (
+    <>
+      <Navebar/>
+      <Product/>
+
+    
+    </>
+  )
+}
+
+export default App
